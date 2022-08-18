f0ed71036136e1d0bcf9fd9f6d5de0041ab71e2b
programCode-sudo <80855480+programCode-sudo@users.noreply.github.com>
2022-08-18 17:20:26 -0600

    Update CMorse.py

--- CMorse.py
+++ CMorse.py
@@ -64,7 +64,7 @@ MORSE_CODE={
     '...---...':'SOS'
 }
 
-TEST='...---...' #Escriba su codigo morse aqui
+TEST='...---...' #Escriba su codigo morse aqui y ejecute la .py
 word=''
 TEST=TEST.strip()    
 TEST=TEST.split('   ')
